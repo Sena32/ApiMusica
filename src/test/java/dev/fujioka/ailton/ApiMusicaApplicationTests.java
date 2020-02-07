b5758d4ab2e2815e16eb402a6fc40c894ee50681
sena32 <seninha.sena9@gmail.com>
2020-02-07 01:49:41 -0300

    Inclusão de Template do Projeto Spring

---
+++ src/test/java/dev/fujioka/ailton/ApiMusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fujioka.ailton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMusicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
